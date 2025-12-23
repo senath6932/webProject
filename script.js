07c50eab4ac2852568b93954064b0901e0087ec9
senath6932 <senathchandira6932@gmail.com>
2025-12-23 18:52:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navigation scroll effect
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 20) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
